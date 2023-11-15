e6f4db5598dd813d38986eb1c71db4356d1064ae
Prof. Nicholas Eduardo Lopes dos Santos <nicholas.santos@ifms.edu.br>
2023-11-15 15:47:48 -0400

    ajuste de configuração para compilar no tomcat

--- src/main/java/br/edu/ifms/websiteadmin/ServletInitializer.java
+++ src/main/java/br/edu/ifms/websiteadmin/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(RpasApplication.class);
+        return application.sources(WebsiteadminApplication.class);
     }
 
 }
